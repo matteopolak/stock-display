51238861253f7d9409ea9216334fde5ff2f8f992
matteopolak <matteopolak@hotmail.com>
2022-01-05 14:44:10 -0500

    remove Debug trait

--- src/structs.rs
+++ src/structs.rs
@@ -1,19 +1,19 @@
 use serde::Deserialize;
 
 #[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 pub struct NasdaqDataWrap {
 	pub data: NasdaqData
 }
 
 #[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 pub struct NasdaqData {
 	pub primaryData: NasdaqPrimaryData
 }
 
 #[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 pub struct NasdaqPrimaryData {
 	pub lastSalePrice: String
 }
